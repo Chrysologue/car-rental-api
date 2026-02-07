5ef3f41ae09ea68c2c43925cd8da4d26d20d6353
Chrysologue <ChrysoBeatz@gmail.com>
2026-02-07 06:04:31 +0300

    updating swagger.js to pull variable from .env

--- swagger.js
+++ swagger.js
@@ -1,15 +1,21 @@
-const swaggerAutogen = require("swagger-autogen")();
+const swaggerAutogen = require('swagger-autogen')();
+require('dotenv').config();
+const isProd = process.env.NODE_ENV === 'production';
+
+const PORT = process.env.PORT || 3000;
 
 const doc = {
   info: {
-    title: "Car rental API",
-    description: "Description",
+    title: 'Car rental API',
+    description: 'Description',
   },
-  host: "localhost:3000",
+  host: isProd ? process.env.PROD_HOST : `localhost:${PORT}`,
+
+  schemes: isProd ? ['https'] : ['http'],
 };
 
-const outputFile = "./swagger.json";
-const routes = ["server.js"];
+const outputFile = './swagger.json';
+const routes = ['server.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
